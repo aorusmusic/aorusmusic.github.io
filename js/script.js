d208de67a3bd7f7efd81d0052fcea066a7d3dae6
Alexander Taipe Apaza <81573976+alexander-taipe@users.noreply.github.com>
2021-07-13 08:47:27 -0500

    Firebase

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-// INCIAR SESION - REGISTRO
-document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
-document.getElementById("btn__registrarse").addEventListener("click", register);
+// // INCIAR SESION - REGISTRO
+// document.getElementById("btn__iniciar-sesion").addEventListener("click", iniciarSesion);
+// document.getElementById("btn__registrarse").addEventListener("click", register);
 
-var contenedor_login_register = document.querySelector(".contenedor__login-register");
-var formulario_login = document.querySelector(".formulario__login");
-var formulario_register = document.querySelector(".formulario__register");
-var caja_trasera_login = document.querySelector(".caja__trasera-login");
-var caja_trasera_register = document.querySelector(".caja__trasera-register");
+// var contenedor_login_register = document.querySelector(".contenedor__login-register");
+// var formulario_login = document.querySelector(".formulario__login");
+// var formulario_register = document.querySelector(".formulario__register");
+// var caja_trasera_login = document.querySelector(".caja__trasera-login");
+// var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
-function iniciarSesion(){
-    formulario_register.style.display = "none";
-    contenedor_login_register.style.left = "10px";
-    formulario_login.style.display = "block";
-    caja_trasera_register.style.opacity = "1";
-    caja_trasera_login.style.opacity = "0"
-}
+// function iniciarSesion(){
+//     formulario_register.style.display = "none";
+//     contenedor_login_register.style.left = "10px";
+//     formulario_login.style.display = "block";
+//     caja_trasera_register.style.opacity = "1";
+//     caja_trasera_login.style.opacity = "0"
+// }
 
-function register(){
-    formulario_register.style.display = "block";
-    contenedor_login_register.style.left = "410px";
-    formulario_login.style.display = "none";
-    caja_trasera_register.style.opacity = "0";
-    caja_trasera_login.style.opacity = "1"
-}
+// function register(){
+//     formulario_register.style.display = "block";
+//     contenedor_login_register.style.left = "410px";
+//     formulario_login.style.display = "none";
+//     caja_trasera_register.style.opacity = "0";
+//     caja_trasera_login.style.opacity = "1"
+// }
